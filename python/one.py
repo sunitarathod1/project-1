d961054cb718bcc7a070df4266458f9aeef41596
sunitarathod1 <sunitarathod0493@gmail.com>
2024-03-05 16:00:54 +0530

    text

---
+++ python/one.py
@@ -0,0 +1,22 @@
+#import random
+#print(random.randrange(1,10))
+a=300
+b=300
+if b>a:
+      print("b is greatrer")
+elif a>b:
+      print("a is greater")
+else:
+      print("a and b are equal")
+
+i=1
+while i<6:
+ print(i)
+ if i==3:
+    break
+ i+=1
+
+fruits=["apple","banana","cherry"]
+for x in "banana":
+     print(x)
+     
---
+++ react/my_react/src/component/Vasu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Vasu(){
+    var movie_name = "kabhi kushi kabhi gum";
+    var movie_actor = "shahruk khan";
+    var movie_actress = "kajol";
+
+    return <div>
+        <h1> Movie_name:{movie_name}</h1>
+        <h2> movie_actor:{movie_actor}</h2>
+        <h3> movie_actress:{movie_actress}</h3>
+    </div>
+
+} 
+export default Vasu
