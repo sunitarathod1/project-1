d6bd04b3ec1a38a42735a9a879e5170034925f69
sunitarathod1 <sunitarathod0493@gmail.com>
2024-03-06 15:55:49 +0530

    text

---
+++ react11/apple/src/App.js
@@ -0,0 +1,16 @@
+
+// import Vasu from "./component/Vasu"
+import Movies from "./component/navbar"
+
+function App(){
+
+
+    return <div>
+        {/* <h1>App Component</h1> */}
+        {/* <hr /> */}
+         <Navbar/> 
+       
+        {/* <Vasu/> */}
+       </div>
+}
+export default App
---
+++ react11/apple/src/component/Movies.jsx
@@ -0,0 +1,17 @@
+
+function Navbar(){
+    // var movie_name = "kabhi kushi kabhi gum";
+    // var movie_actor = "shahruk khan";
+    //var movie_actress = "kajol";
+
+    return <nav className= "navbar navbar-dark bg-dark">
+          <a href="#" className="navbar-brand">Logo</a>    
+             </nav>
+     
+} 
+export default Navbar
+
+
+
+
+
